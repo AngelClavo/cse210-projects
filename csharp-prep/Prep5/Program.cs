a69a9b630ce8c0b1a7ebee457ed841590adedc44
yourusername <youremail@>
2024-09-22 23:47:22 -0500

    Prep5 done

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -5,5 +5,42 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep5 World!");
+
+        display_welcome();
+        string name = written_name();
+        int number = written_number();
+        int number_square = new_number(number);
+        answer(name, number_square);
     }
+
+    static void display_welcome()
+    {
+        Console.WriteLine("Welcome to the program");
+    }
+
+    static string written_name()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int written_number()
+    {
+        Console.Write("Enter the number you chose, please: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+
+    static int new_number(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    static void answer(string name, int number_square)
+    {
+        Console.WriteLine($"{name}, the square of your number is: {number_square}");
+    }
+
 }
